fe7745b2394d08e9a2d01845ccad8e3b457d6d0c
GoncaloSoaress <goncalo.soares01@ua.pt>
2023-05-02 10:30:02 +0100

    Update movie.h

---
+++ libs/movie.c
@@ -0,0 +1,8 @@
+/** @file movie.c
+ * @brief Definition of the struct data and the functions prototypes for movie.h
+ * 
+ * 
+ * @author Gonçalo Soares & Gonçalo Rodrigues
+ * @date 12 March 2023
+ * @bug No known bugs.
+ */
